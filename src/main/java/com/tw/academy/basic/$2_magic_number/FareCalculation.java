b0d07b0158a33cdc4109f624eb8ff0335ab665b3
Alice <alice.w.s.li@oocl.com>
2021-06-22 10:57:39 +0800

    refactor: introduce the constant

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,12 +2,19 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    public static final int min_eight_percent_off_consumption = 100;
+    public static final int max_eight_percent_off_consumption = 150;
+    public static final int min_five_percent_off_consumpption = 150;
+    public static final int max_five_percent_off_consumption = 400;
+    public static final double eight_percent_off = 0.8;
+    public static final double five_percent_off = 0.5;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (cumulativeExpensesThisMonth >= min_eight_percent_off_consumption && cumulativeExpensesThisMonth < max_eight_percent_off_consumption) {
+            return originalPrice * eight_percent_off;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (cumulativeExpensesThisMonth >= min_five_percent_off_consumpption && cumulativeExpensesThisMonth < max_five_percent_off_consumption) {
+            return originalPrice * five_percent_off;
         }
         return originalPrice;
     }
