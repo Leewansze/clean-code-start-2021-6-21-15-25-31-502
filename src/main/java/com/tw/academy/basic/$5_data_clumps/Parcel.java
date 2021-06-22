4f005b83524e3fcf4e809bad48d471afc64c1815
Alice <alice.w.s.li@oocl.com>
2021-06-22 15:24:47 +0800

    refactor: make field &inline sender field

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -1,28 +1,31 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class Parcel {
+
     private String size;
     private Double weight;
 
-    private String senderName;
-    private String senderPhoneNumber;
-    private String senderAddress;
-    private User received;
+    private final String senderName;
+    private final String senderPhoneNumber;
+    private final String senderAddress;
+    private final User receiver;
+    private final User sender;
 
     public Parcel(String size, Double weight,
-                  User received, User sender) {
-        this.received = received;
+                  User receiver, User sender) {
+        this.receiver = receiver;
         this.size = size;
         this.weight = weight;
-        this.senderName = sender.getName();
-        this.senderPhoneNumber = sender.getPhoneNumber();
-        this.senderAddress = sender.getAddress();
+        this.sender = sender;
+        this.senderName = this.sender.getName();
+        this.senderPhoneNumber = this.sender.getPhoneNumber();
+        this.senderAddress = this.sender.getAddress();
     }
 
     public String confirmReceiver(){
         return String.format("Please confirm receiver information: receiver name is %s, " +
                         "receiver phone number is %s and receiver address is %s.\n",
-                received.getName(), received.getPhoneNumber(), received.getAddress());
+                receiver.getName(), receiver.getPhoneNumber(), receiver.getAddress());
     }
 
     public String confirmSender(){
