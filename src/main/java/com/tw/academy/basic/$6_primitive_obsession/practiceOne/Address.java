aa95ae04fa484177a6115204b1db63e2ceac4109
Alice <alice.w.s.li@oocl.com>
2021-06-22 15:53:07 +0800

    refactor: new Address Class

---
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -0,0 +1,14 @@
+package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
+
+public class Address {
+    private String province;
+    private String city;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
